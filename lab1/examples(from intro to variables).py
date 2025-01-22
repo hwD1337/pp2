17124ea7ecaf68e7b8206c6c0b8e625858c26a67
hwD1337 <94920113+hwD1337@users.noreply.github.com>
2025-01-22 15:49:28 +0500

    Create examples(from intro to variables).py

---
+++ lab1/examples(from intro to variables).py
@@ -0,0 +1,21 @@
+x = 5
+y = "John"
+a = str(3)    # x will be '3'
+b = int(3)    # y will be 3
+c = float(3)  # z will be 3.0
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+if 5 > 2:
+  print("Five is greater than two!")
+print("Hello, World!")
+print(x)
+print(y)
+print(a)
+print(b)
+print(c)
+print(type(x))
+print(type(y))
