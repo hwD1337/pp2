367ce757ce507e1a65eb5ead6aababe0e9129e45
hwD1337 <94920113+hwD1337@users.noreply.github.com>
2025-01-22 16:14:31 +0500

    Create casting.py

---
+++ lab1/casting.py
@@ -0,0 +1,8 @@
+x = int(18)   # x will be 18
+y = int(6.5) # y will be 6
+z = int("3") # z will be 3
+
+x = float(9)     # x will be 1.0
+y = float(6.2)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("5.9") # w will be 4.2
