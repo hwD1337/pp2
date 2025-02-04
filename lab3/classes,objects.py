df2ba9f91d3ed74c44ca4ad669f4e61e7e840666
hwD1337 <94920113+hwD1337@users.noreply.github.com>
2025-02-05 01:54:30 +0500

    Create classes,objects.py

---
+++ lab3/classes,objects.py
@@ -0,0 +1,50 @@
+#Create a class named MyClass, with a property named x:
+class MyClass:
+  x = 15
+
+#Create a class named Person, use the __init__() function to assign values for name and age:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Michael", 24)
+
+print(p1.name)
+print(p1.age)
+
+#The string representation of an object WITHOUT the __str__() function:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+#The result will be <__main__.Person object at 0x15039e602100>
+
+#The string representation of an object WITH the __str__() function:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+#Insert a function that prints a greeting, and execute it on the p1 object: Creating method in the class
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
