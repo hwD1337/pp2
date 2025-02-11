ecd895da008a93f3008db16b883c50456a289f66
hwD1337 <94920113+hwD1337@users.noreply.github.com>
2025-02-11 21:32:58 +0500

    Create Math.py

---
+++ lab4/Math.py
@@ -0,0 +1,23 @@
+x = min(5, 15, 40)
+y = max(5, 10, 50)
+
+print(x)
+print(y)
+
+#make positive 
+x = abs(-15.34)
+
+print(x)
+
+#Return the value of 5 to the power of 6:
+x = pow(5, 6)
+
+print(x)
+
+#d
+import math
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
